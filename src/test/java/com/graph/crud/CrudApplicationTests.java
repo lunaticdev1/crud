944df1f50dc082cd6239c622d756c20dcd04fd8e
lunaticDev <thelunaticdev@gmail.com>
2024-09-30 17:50:10 -0400

    Initial commit

---
+++ src/test/java/com/graph/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graph.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
